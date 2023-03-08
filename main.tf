19dbf8ab1caa8602ee72bf06ad90de448d5c21fb
renelopezruiz <126912880+renelopezruiz@users.noreply.github.com>
2023-03-08 17:52:31 -0600

    new vpc

--- main.tf
+++ main.tf
@@ -60,7 +60,7 @@ resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in, allow everything out"
   
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.vpc.public_subnets
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
